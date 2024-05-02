294fda3eab4f70639bab12720cab50ac732340b5
Mysti <mystievous@gmail.com>
2024-05-02 15:35:37 -0700

    add helpers to hide tooltip info

--- src/main/java/io/github/mystievous/mysticore/ItemUtil.java
+++ src/main/java/io/github/mystievous/mysticore/ItemUtil.java
@@ -3,10 +3,15 @@ package io.github.mystievous.mysticore;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
 
 public class ItemUtil {
 
@@ -34,6 +39,28 @@ public class ItemUtil {
         } else {
             return Component.translatable(item.translationKey());
         }
+    }
+
+    public static void hideAttributes(ItemStack itemStack) {
+        itemStack.editMeta(itemMeta -> {
+            itemMeta.addAttributeModifier(Attribute.GENERIC_LUCK, new AttributeModifier(UUID.randomUUID(), "HideAttributes", 0, AttributeModifier.Operation.ADD_NUMBER));
+            itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        });
+    }
+
+    public static void hideExtraTooltip(ItemStack itemStack) {
+        hideAttributes(itemStack);
+        itemStack.addItemFlags(
+                ItemFlag.HIDE_ATTRIBUTES,
+                ItemFlag.HIDE_ADDITIONAL_TOOLTIP,
+                ItemFlag.HIDE_DYE,
+                ItemFlag.HIDE_ENCHANTS,
+                ItemFlag.HIDE_ARMOR_TRIM,
+                ItemFlag.HIDE_DESTROYS,
+                ItemFlag.HIDE_PLACED_ON,
+                ItemFlag.HIDE_STORED_ENCHANTS,
+                ItemFlag.HIDE_UNBREAKABLE
+        );
     }
 
 }
